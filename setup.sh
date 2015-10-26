1396a63b65690ab2bf6593981216327b24f5a068
turanegaku <turanegaku@gmail.com>
2015-10-26 16:46:05 +0900

    not worked

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-DOT_FILES=( .gitconfig .tmux.conf .vimperatorrc .vimrc .zshrc )
+DOT_FILES=(.gitconfig .tmux.conf .vimperatorrc .vimrc .zshrc)
 
 for file in ${DOT_FILES[@]}; do
-  ln -is "`dirname $0`/${file}" ${HOME}
+  ln -is "$HOME/dotfiles/$file" $HOME
 done
