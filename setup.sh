7d6385dd94bb293cc043b1e9da93a5c02b959ed4
turanegaku <turanegaku@gmail.com>
2015-12-10 13:43:23 +0900

    upgrade setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@
 DOT_FILES=(.gitconfig .tmux.conf .vimperatorrc .vimrc .zshrc .uncrustifyconfig .vrapperrc)
 
 for file in ${DOT_FILES[@]}; do
-  if [[ `md5 -q $HOME/$file` != `md5 -q $HOME/dotfiles/$file` ]]; then
+  if [[ -e $HOME/$file ]]; then pmd5=`md5 -q $HOME/$file`; fi
+  if [[ -e $HOME/dotfiles/$file ]]; then md5=`md5 -q $HOME/dotfiles/$file`; fi
+  if [[ $pmd5 != $md5 ]]; then
     ln -is "$HOME/dotfiles/$file" $HOME
   fi
 done
