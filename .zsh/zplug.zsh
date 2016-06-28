2fc8983fc8202f3e638f6457ec28b45d7e3a5db8
turanegaku <turanegaku@gmail.com>
2016-06-29 04:43:54 +0900

    install gomi peco fzf

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -1,4 +1,7 @@
 zplug "~/.zsh", from:local, use:"<->_*.zsh"
 zplug "b4b4r07/enhancd", use:init.sh
+zplug "b4b4r07/zsh-gomi", as:command, use:bin/gomi
+zplug "peco/peco", as:command, from:gh-r, frozen:1
+zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:"fzf", frozen:1
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", nice:19
