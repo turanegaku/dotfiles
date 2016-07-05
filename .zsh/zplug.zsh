8e66b3dbe8917d3e840547c9eb4cd57147bd5651
turanegaku <turanegaku@gmail.com>
2016-07-05 15:06:57 +0900

    Revert "gomi install only if there is /bin/zsh"
    This reverts commit 2c4f4b44b949a5b92d00f947f85c9c45dadbf01b.

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -1,10 +1,8 @@
 zplug "~/.zsh", from:local, use:"<->0_*.zsh"
 zplug "~/.zsh", from:local, use:"<->5_*.zsh", nice:15
 
-if [[ -f /bin/zsh ]]; then
-  zplug "b4b4r07/zsh-gomi", as:command, use:bin/gomi
-fi
 zplug "b4b4r07/enhancd", use:init.sh
+zplug "b4b4r07/zsh-gomi", as:command, use:bin/gomi
 zplug "peco/peco", as:command, from:gh-r, frozen:1
 zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:"fzf", frozen:1
 
