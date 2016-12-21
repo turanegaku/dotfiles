b326319d5a84f55eb690dea8b6eca0c34a7bde3b
turanegaku <turanegaku@gmail.com>
2016-12-22 00:09:38 +0900

    add ghq

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -6,6 +6,8 @@ zplug "b4b4r07/zsh-gomi", as:command, use:bin/gomi
 zplug "peco/peco", as:command, from:gh-r, frozen:1
 zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:"fzf", frozen:1
 
+zplug "motemen/ghq", as:command, from:gh-r, rename-to:ghq, frozen:1
+
 zplug "~/bin", from:local, as:command, use:rpp.sh, rename-to:"rpp"
 
 zplug "zsh-users/zsh-completions"
